b18e9badbbc752a9d844ca38efad6405b04237f8
Mikhail <mla812@mail.ru>
2020-05-07 23:10:21 +0300

    Add Buttons and Logic

--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -9,8 +9,40 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+
+    @State var alertIsVisible: Bool = false
+    //Remember to delete this later on!
+    //@State var whosThereIsVisible: Bool = true
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            VStack {
+                Text("Welcome to my first app!")
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.green)
+                Button(action: {
+                   print("Button pressed")
+                    self.alertIsVisible = true
+                }) {
+                    Text(/*@START_MENU_TOKEN@*/"Hit Me!"/*@END_MENU_TOKEN@*/)
+                }
+                .alert(isPresented: $alertIsVisible) { () ->
+                    Alert in
+                    return Alert(title: Text("Hello there!"), message: Text("This is my first popup"), dismissButton: .default(Text("Awesome")))
+                    
+                }
+            }
+            /*Button(action: {
+                self.whosThereIsVisible=true
+            }) {
+                Text(/*@START_MENU_TOKEN@*/"Knock Knock"/*@END_MENU_TOKEN@*/)
+            }
+            .alert(isPresented: $whosThereIsVisible) { () ->
+                Alert in
+                return Alert(title: Text("Who's there?"), message: Text("Little old lady"),
+                             dismissButton: .default(Text("Little old lady who !?")))
+        }*/
     }
 }
 
@@ -18,4 +50,5 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
 }
