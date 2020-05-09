826e98b6dfe7f5b6837a8fe9e1e5e03874f16e68
Mikhail <mla812@mail.ru>
2020-05-09 22:50:11 +0300

    Add Basic Game Logic

--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -12,31 +12,70 @@ struct ContentView: View {
     
 
     @State var alertIsVisible: Bool = false
+    @State var sliderValue: Double = 50.0
     
     var body: some View {
-        VStack {
             VStack {
-                Text("Welcome to my first app!")
-                    .fontWeight(.bold)
-                    .foregroundColor(Color.green)
+                Spacer()
+                
+                // Target row
+                HStack {
+                    VStack {
+                        HStack {
+                            Text("Put the bullseye as close you can to:")
+                            Text("100")
+                        }
+                    }
+
+                }
+                Spacer()
+                
+                // Slider row
+                HStack {
+                    Text("1")
+                    Slider(value: self.$sliderValue, in: 1...100)
+                }
+                Spacer()
+                
+                // Button row
                 Button(action: {
                    print("Button pressed")
                     self.alertIsVisible = true
                 }) {
                     Text(/*@START_MENU_TOKEN@*/"Hit Me!"/*@END_MENU_TOKEN@*/)
                 }
-                .alert(isPresented: $alertIsVisible) { () ->
-                    Alert in
-                    return Alert(title: Text("Hello there!"), message: Text("This is my first popup"), dismissButton: .default(Text("Awesome")))
+                .alert(isPresented: $alertIsVisible) { () -> Alert in
+                    var roundedValue: Int = Int(self.sliderValue.rounded())
+                    return Alert(title: Text("Hello there!"), message: Text("The slider's value is \(roundedValue)."), dismissButton: .default(Text("Awesome")))
                     
                 }
-            }
+                Spacer()
+                
+                // Score row
+                HStack {
+                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                        Text(/*@START_MENU_TOKEN@*/"Start over"/*@END_MENU_TOKEN@*/)
+                    }
+                    Spacer()
+                    Text("Score:")
+                    Text("999999")
+                    Spacer()
+                    Text("Round:")
+                    Text("999")
+                    Spacer()
+                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                    Text("Info")
+                    }
+                }
+                .padding(.bottom, 20)
+        }
+        
     }
-}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView().previewLayout(.fixed(width: 896, height: 414))
     }
 }
 }
